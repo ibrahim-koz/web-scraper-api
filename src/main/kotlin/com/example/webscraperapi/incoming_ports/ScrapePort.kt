c1881d7115772bd0080762c12a800ae6bf222197
ibrahimkoz <ibrahimkoz.cs@gmail.com>
2022-08-24 13:20:59 +0300

    Make available domain names that can be scraped

--- src/main/kotlin/com/example/webscraperapi/incoming_ports/ScrapePort.kt
+++ src/main/kotlin/com/example/webscraperapi/incoming_ports/ScrapePort.kt
@@ -2,14 +2,17 @@ package com.example.webscraperapi.incoming_ports
 
 import com.example.webscraperapi.domain.use_cases.scrape
 import com.example.webscraperapi.requests.ScrapeRequest
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("api/")
+@RequestMapping("api/scrape")
 class ScrapePort {
+    @GetMapping
+    fun get() = listOf("https://peachscore.com/", "https://slack.com/")
     @PostMapping
     fun post(@RequestBody scrapeRequest: ScrapeRequest) = scrape(scrapeRequest.path)
 }
